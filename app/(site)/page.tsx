88c6a1f936761b17bfd407976e8547f0f1efaff3
Pablo George <pablogeokar@hotmail.com>
2023-05-21 08:36:16 -0300

    02 AuthUI

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import AuthForm from "./components/AuthForm";
 
 export default function Home() {
   return (
@@ -6,6 +7,7 @@ export default function Home() {
       className="
         flex
         min-h-full
+        flex-col
         justify-center
         py-12
         sm:px-6
@@ -34,7 +36,7 @@ export default function Home() {
           Sign in to your account!
         </h2>
       </div>
-      {/* AuthForm */}
+      <AuthForm />
     </div>
   );
 }
